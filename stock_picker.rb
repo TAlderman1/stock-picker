59aeac597fde8175c097060dc9ba3807a439b2f2
TAlderman1 <52610092+TAlderman1@users.noreply.github.com>
2020-09-05 16:53:09 -0400

    Stock Picker code file upload

---
+++ stock_picker.rb
@@ -0,0 +1,26 @@
+def stock_picker(arr)
+
+    #sets up profit tracking variable and answer array
+    profit = 0
+    answer = []
+    
+    #for each item in the array, scan through each following integer and subtract from "buy"
+    arr.each{ |buy|         
+        start = arr.index(buy) + 1
+        arr[start..-1].each{ |sell|  
+
+            #if the result of buy-sell > profit = true, return new profit number and new array of [buy, sell] prices
+            if sell - buy > profit      
+                profit = sell - buy
+                answer.clear()
+                answer.push(buy, sell)
+            end       
+        }
+    }
+    #convert the answers for [buy,sell] into their appropriate index values in arr
+    answer.map!{ |a| arr.index(a) }
+    p answer
+end
+
+stock_picker([17,3,6,9,15,8,6,1,10])
+
